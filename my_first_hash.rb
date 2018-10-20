fdce0960a476da969647d1606f585822ca1fea74
Cristian  CC <cristi.nord@gmail.com>
2018-10-20 13:45:21 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,16 +4,16 @@ end
 
 
 def shipping_manifest
- the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+ return the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
-                  whale bone corsets 
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
+  return shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
